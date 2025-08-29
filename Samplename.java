b7f9877db3f74e5faa0103c7ada09093ffdce50b
alekya0906 <alekyabudi@gmail.com>
2025-08-29 19:58:20 +0530

    Samplename.java

--- Samplename.java
+++ Samplename.java
@@ -1,6 +1,6 @@
 public class Samplename{
     public static void main(string[] args){
-        String name ="alekya";
+        string name ="alekya";
         System.out.Println("hello,my name is alekya"+name);
     }
 }
